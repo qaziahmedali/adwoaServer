26ed7d027380ca9ae0d6fca622b1ac4fa7188240
mirzanouman <mirzamuzammil992@gmail.com>
2022-06-29 19:23:33 +0500

    done changes

--- config/dev.js
+++ config/dev.js
@@ -3,8 +3,8 @@ module.exports = {
   DEBUG_MODE: true,
   DB_URL:
     "mongodb+srv://admin:admin123@cluster0.i8hfj.mongodb.net/adwoaDB?retryWrites=true&w=majority",
-  ADWOA_JWT_SECRET: "MYNAMEISNOUMANMUZAMMIL",
-  ADWOA_REFRESH_SECRET: "NOUMANMUZAMMILTHISISREFRESHTOKEN",
+  JWT_SECRET: "ADWOAJWTSECRET",
+  REFRESH_SECRET: "ADWOAREFRESHSECRET",
   FIREBASE_TOKEN:
     "c5EIA4MdTfGKCjRctt8tUX:APA91bFPoBw4w6uOokgPxdTHTeOse_P0k98H-y5f2uTHDOXT-d_lo7qNiK9fjg9CFAwOkIkKGc8xqpFJj3VhrCdcY-ucL5WPvqmc2TE--H9FLXQlhthwBq5ldVEOAr1odRbFZXXmeC5F",
 };
