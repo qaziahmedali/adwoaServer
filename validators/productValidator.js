7d0b8d3b1fa8601daf8bc64102be76919048e113
mirzanouman <mirzamuzammil992@gmail.com>
2022-07-06 12:42:40 +0500

    done

--- validators/productValidator.js
+++ validators/productValidator.js
@@ -4,7 +4,7 @@ const productSchema = Joi.object({
   name: Joi.string().required(),
   price: Joi.string().required(),
   category: Joi.string().required(),
-  seller: Joi.string().required(),
+  user: Joi.string().required(),
   location: Joi.string().required(),
   payment: Joi.string().required(),
   des: Joi.string().required(),
